1d16c2c78756670c875e725489728aff7e47cb6f
Liclic <Burulevap@gmail.com>
2016-02-13 20:40:07 +0400

    создал интерфейс AccountDAO

--- src/main/java/model/dao/AccountDAO.java
+++ src/main/java/model/dao/AccountDAO.java
@@ -1,7 +1,10 @@
 package model.dao;
 
+import model.Account;
+import org.springframework.data.repository.CrudRepository;
 /**
  * Created by Алексей on 13.02.2016.
  */
-public interface AccountDAO {
+
+public interface AccountDAO extends CrudRepository<Account,Long>{
 }
